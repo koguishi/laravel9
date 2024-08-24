aef539fa29dfa3aa9b634d4c807e6e55752c6284
Edson Koguishi <edson.koguishi@amaiseducacao.com.br>
2024-08-24 14:26:59 -0300

    test: read CategoriaRepository

--- tests/Feature/repository/eloquent/CategoriaRepositoryTest.php
+++ tests/Feature/repository/eloquent/CategoriaRepositoryTest.php
@@ -40,4 +40,43 @@ class CategoriaRepositoryTest extends TestCase
             'created_at' => $entity->criadoEm(),
         ]);
     }
+
+    public function testRead()
+    {
+        $categoria1 = New CategoriaEntity(
+            nome: 'Treinos',
+            descricao: 'Vídeo de treinos',
+            ativo: true,
+        );
+        $this->repository->create($categoria1);
+        $categoria2 = New CategoriaEntity(
+            nome: 'Competições',
+            descricao: 'Vídeo de competições',
+            ativo: true,
+        );
+        $this->repository->create($categoria2);
+
+        $this->assertDatabaseHas('categorias', [
+            'nome' => $categoria1->nome,
+            'descricao' => $categoria1->descricao,
+            'ativo' => $categoria1->ativo,
+            'created_at' => $categoria1->criadoEm(),
+        ]);
+        $this->assertDatabaseHas('categorias', [
+            'nome' => $categoria2->nome,
+            'descricao' => $categoria2->descricao,
+            'ativo' => $categoria2->ativo,
+            'created_at' => $categoria2->criadoEm(),
+        ]);
+        $response = $this->repository->read($categoria1->id);
+
+        $this->assertInstanceOf(CategoriaEntity::class, $response);
+        $this->assertDatabaseHas('categorias', [
+            'nome' => $categoria1->nome,
+            'descricao' => $categoria1->descricao,
+            'ativo' => $categoria1->ativo,
+            'created_at' => $categoria1->criadoEm(),
+        ]);
+    }
+
 }
