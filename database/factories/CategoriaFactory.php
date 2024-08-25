a8a362bfd753c43a8dcab7701865a121f96075f6
Edson Koguishi <edson.koguishi@amaiseducacao.com.br>
2024-08-25 15:26:55 -0300

    fea: CategoriaFactory

---
+++ database/factories/CategoriaFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Categoria>
+ */
+class CategoriaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'id' => (string) Str::uuid(),
+            'nome' => $this->faker->name(),
+            'descricao' => $this->faker->sentence(10),
+            'ativo' => true,        ];
+    }
+}
