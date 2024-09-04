cf5fc3a968f574d46b87b235228549c6d960d659
Edson Koguishi <edson.koguishi@amaiseducacao.com.br>
2024-09-03 21:36:07 -0300

    test: CategoriaController unit

--- src/core/usecase/categoria/ReadAllCategoriasInput.php
+++ src/core/usecase/categoria/ReadAllCategoriasInput.php
@@ -7,5 +7,7 @@ class ReadAllCategoriasInput
     public function __construct(
         public string $filter = '',
         public array $arrOrder = [],
+        public int $page = 1,
+        public int $totalPage = 15,
     ) { }
 }
---
+++ tests/Unit/app/Http/Controllers/CategoriaControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Unit\app\Http\Controllers;
+
+use App\Http\Controllers\CategoriaController;
+use core\usecase\categoria\ReadAllCategoriasInput;
+use core\usecase\categoria\ReadAllCategoriasOutput;
+use core\usecase\categoria\ReadAllCategoriasUsecase;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+
+class CategoriaControllerTest extends TestCase
+{
+    public function testIndex()
+    {
+        $inputDto = new ReadAllCategoriasInput(
+            filter: 'filtro',
+            arrOrder: ['nome' => 'ASC'],
+            page: 1,
+            totalPage: 15
+        );        
+        /**
+         * @var Request
+         */
+        $mockRequest = Mockery::mock(Request::class);
+        $mockRequest->shouldReceive('get')->andReturn('teste');
+        $mockRequest->shouldReceive('input')->andReturn($inputDto);
+
+        $mockOutputDto = Mockery::mock(ReadAllCategoriasOutput::class, [
+            [], 1, 1, 1, 1, 1, 1, 1,
+        ]);
+        $mockOutputDto = new ReadAllCategoriasOutput([]);
+
+        /**
+         * @var ReadAllCategoriasUsecase|MockInterface
+         */
+        $mockUsecase = Mockery::mock(ReadAllCategoriasUsecase::class);
+        $mockUsecase->shouldReceive('execute')->andReturn($mockOutputDto);
+
+        $controller = new CategoriaController();
+        $response = $controller->index($mockRequest, $mockUsecase);
+
+        dump($response);
+
+        $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
+
+        $this->assertIsObject($response->resource);
+        // $this->assertArrayHasKey('meta', $response->additional);        
+    }
+}
