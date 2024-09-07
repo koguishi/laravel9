3531ac4a297ffcc1fdc346d8637bd83eb1803a28
Edson Koguishi <edson.koguishi@amaiseducacao.com.br>
2024-09-07 08:42:53 -0300

    test: red integracao categoria store

--- tests/Feature/http/controllers/CategoriaControllerTest.php
+++ tests/Feature/http/controllers/CategoriaControllerTest.php
@@ -5,12 +5,15 @@ namespace Tests\Feature\http\controllers;
 use App\Http\Controllers\CategoriaController;
 use App\Models\Categoria as CategoriaModel;
 use app\repository\eloquent\CategoriaRepository;
+use core\usecase\categoria\CreateCategoriaUsecase;
 use core\usecase\categoria\PaginateCategoriasUsecase;
 use core\usecase\categoria\ReadAllCategoriasUsecase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 use Tests\TestCase;
 
 class CategoriaControllerTest extends TestCase
@@ -38,5 +41,17 @@ class CategoriaControllerTest extends TestCase
         $this->assertInstanceOf(AnonymousResourceCollection::class, $response);
         $this->assertArrayHasKey('meta', $response->additional);
     }
+
+    public function test_store()
+    {
+        $useCase = new CreateCategoriaUsecase($this->repository);
+
+        $request = new StoreCategoryRequest();
+
+        $response = $this->controller->store($request, $useCase);
+
+        $this->assertInstanceOf(JsonResponse::class, $response);
+        $this->assertEquals(Response::HTTP_CREATED, $response->status());
+    }    
 
 }
