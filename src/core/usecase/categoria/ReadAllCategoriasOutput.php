5d457adf6a085dda66a4f9024bf6ffbcaf6e03f8
Edson Koguishi <edson.koguishi@amaiseducacao.com.br>
2024-09-07 08:23:05 -0300

    undo: usecase ReadAll não precisa informacoes de paginacao

--- src/core/usecase/categoria/ReadAllCategoriasOutput.php
+++ src/core/usecase/categoria/ReadAllCategoriasOutput.php
@@ -6,12 +6,5 @@ class ReadAllCategoriasOutput
 {
     public function __construct(
         public array $items,
-        public int $total,
-        public int $currentPage,
-        public int $lastPage,
-        public int $firstPage,
-        public int $perPage,
-        public int $to,
-        public int $from,
     ) { }
 }
