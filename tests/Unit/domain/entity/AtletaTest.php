dc380fccb4151ebaa6b08d976fe9350b359ba5db
edson koguishi <edsonkoguishi@gmail.com>
2024-09-17 23:00:50 -0300

    test: dtNascimento deve ser antes de hoje

--- tests/Unit/domain/entity/AtletaTest.php
+++ tests/Unit/domain/entity/AtletaTest.php
@@ -6,6 +6,7 @@ use core\domain\entity\Atleta;
 use core\domain\exception\EntityValidationException;
 use core\domain\valueobject\Uuid;
 use DateTime;
+use DateTimeZone;
 use PHPUnit\Framework\TestCase;
 use Ramsey\Uuid\Uuid as RamseyUuid;
 
@@ -73,6 +74,21 @@ class AtletaTest extends TestCase
         } catch (\Throwable $th) {
             $this->assertInstanceOf(EntityValidationException::class, $th);
             $this->assertEquals("Nome deve ter no máximo 100 caracteres", $th->getMessage());
+        }
+    }
+    
+    public function testExceptionDtNascimentoHojeEmDiante()
+    {
+        try {
+            $atleta = new Atleta(
+                nome: random_bytes(100),
+                dtNascimento: new DateTime(today()),
+            );
+
+            $this->assertTrue(false);
+        } catch (\Throwable $th) {
+            $this->assertInstanceOf(EntityValidationException::class, $th);
+            // $this->assertEquals("Nome deve ter no máximo 100 caracteres", $th->getMessage());
         }
     }
 
