e0963cd4f401e81cef8526bd2a88fbcbe710d394
edson koguishi <edsonkoguishi@gmail.com>
2024-09-17 22:24:25 -0300

    feat: validar NomeMenorQue3

--- src/core/domain/entity/Atleta.php
+++ src/core/domain/entity/Atleta.php
@@ -3,6 +3,7 @@
 namespace core\domain\entity;
 
 use core\domain\entity\traits\MagicMethodsTrait;
+use core\domain\validation\DomainValidation;
 use core\domain\valueobject\Uuid;
 use DateTime;
 
@@ -18,5 +19,15 @@ class Atleta
     ) {
         $this->id = $this->id ?? Uuid::random();
         $this->criadoEm = $this->criadoEm ?? new DateTime();
+        $this->validate();
+    }
+
+    private function validate()
+    {
+        $nomeMinLen = 3;
+        DomainValidation::strMinLen($this->nome, $nomeMinLen, "Nome deve ter no mínimo {$nomeMinLen} caracteres");
+        $nomeMaxLen = 100;
+        DomainValidation::strMaxLen($this->nome, $nomeMaxLen, "Nome deve ter no máximo {$nomeMaxLen} caracteres");
+        // DomainValidation::notNull($this->dtNascimento)
     }
 }
