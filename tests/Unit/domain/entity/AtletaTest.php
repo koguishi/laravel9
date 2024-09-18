a6a83a9ed13e949659d703d0f44c2c5d3a752451
edson koguishi <edsonkoguishi@gmail.com>
2024-09-17 22:24:10 -0300

    test: validar NomeMenorQue3

--- tests/Unit/domain/entity/AtletaTest.php
+++ tests/Unit/domain/entity/AtletaTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit\domain\entity;
 
 use core\domain\entity\Atleta;
+use core\domain\exception\EntityValidationException;
 use core\domain\valueobject\Uuid;
 use DateTime;
 use PHPUnit\Framework\TestCase;
@@ -43,5 +44,20 @@ class AtletaTest extends TestCase
         $this->assertEquals($dtNascimento, $atleta->dtNascimento);
         $this->assertEquals($uuid, $atleta->id());
         $this->assertEquals($criadoEm, $atleta->criadoEm);
+    }
+
+    public function testExceptionNomeMenorQue3()
+    {
+        try {
+            new Atleta(
+                nome: 'AA',
+                dtNascimento: new DateTime(),
+            );
+
+            $this->assertTrue(false);
+        } catch (\Throwable $th) {
+            $this->assertInstanceOf(EntityValidationException::class, $th);
+            $this->assertEquals("Nome deve ter no mínimo 3 caracteres", $th->getMessage());
+        }
     }
 }
