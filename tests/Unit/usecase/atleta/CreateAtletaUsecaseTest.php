0621de11b7295c227fe1118c170bc8ac1bbd6282
edson koguishi <edsonkoguishi@gmail.com>
2024-09-19 21:27:39 -0300

    test: CreateAtletaUsecase

---
+++ tests/Unit/usecase/atleta/CreateAtletaUsecaseTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Tests\Unit\usecase\atleta;
+
+use core\domain\entity\Atleta;
+use core\domain\repository\AtletaRepositoryInterface;
+use core\usecase\atleta\CreateAtletaInput;
+use core\usecase\atleta\CreateAtletaOutput;
+use core\usecase\atleta\CreateAtletaUsecase;
+use DateTime;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+
+class CreateAtletaUsecaseTest extends TestCase
+{
+    public function tearDown(): void
+    {
+        Mockery::close();
+    }
+
+    public function testCreateAtleta()
+    {
+        $atleta = new Atleta(
+            nome: 'categoria A',
+            dtNascimento: new DateTime('2001-01-01'),
+        );
+
+        /**
+         * @var AtletaRepositoryInterface|MockInterface $mockRepo
+         */
+        $mockRepo = Mockery::mock(
+            stdClass::class,
+            AtletaRepositoryInterface::class,
+        );
+        $mockRepo->shouldReceive('create')->andReturn($atleta);
+
+        $input = new CreateAtletaInput(
+            nome: $atleta->nome,
+            dtNascimento: $atleta->dtNascimento,
+        );
+
+        $usecase = new CreateAtletaUsecase($mockRepo);
+        $response = $usecase->execute($input);
+
+        $mockRepo->shouldHaveReceived('create');
+
+        $this->assertInstanceOf(CreateAtletaOutput::class, $response);
+        $this->assertEquals($atleta->id(), $response->id);
+        $this->assertEquals($atleta->nome, $response->nome);
+        $this->assertEquals($atleta->dtNascimento, $response->dtNascimento);
+        $this->assertNotEmpty($atleta->criadoEm(), $response->criadoEm);
+    }
+}
