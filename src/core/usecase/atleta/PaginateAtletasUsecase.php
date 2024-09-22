06d8a31b3b03e9456722400270870eed4cc9203b
edson koguishi <edsonkoguishi@gmail.com>
2024-09-22 10:25:19 -0300

    feat: PaginateAtletasUsecase

---
+++ src/core/usecase/atleta/PaginateAtletasUsecase.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace core\usecase\atleta;
+
+use core\domain\repository\AtletaRepositoryInterface;
+
+class PaginateAtletasUsecase
+{
+    protected AtletaRepositoryInterface $repository;
+
+    public function __construct(AtletaRepositoryInterface $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function execute(PaginateAtletasInput $input): PaginateAtletasOutput
+    {
+        $atletas = $this->repository->paginate(
+            filter: $input->filter,
+            order: $input->order,
+            page: $input->page,
+            totalPage: $input->totalPage,
+        );
+
+        return new PaginateAtletasOutput(
+            items: array_map(function ($data) {
+                return [
+                    'id' => $data->id,
+                    'nome' => $data->nome,
+                    'dtNascimento' => $data->dtNascimento,
+                    'criadoEm' => $data->criadoEm,
+                ];
+            }, $atletas->items()),
+            total: $atletas->total(),
+            current_page: $atletas->currentPage(),
+            last_page: $atletas->lastPage(),
+            first_page: $atletas->firstPage(),
+            per_page: $atletas->perPage(),
+            to: $atletas->to(),
+            from: $atletas->from(),
+        );
+    }
+}
