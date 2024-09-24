ad68448c732cee4c7e4268c7bf0bd2fbd3c948bf
edson koguishi <edsonkoguishi@gmail.com>
2024-09-24 19:08:54 -0300

    feat: model, factory e migration de atleta

---
+++ app/Models/Atleta.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Atleta extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'id',
+        'nome',
+        'dtNascimento',
+    ];
+
+    protected $casts = [
+        'id' => 'string'
+    ];
+
+    public $incrementing = false;
+}
---
+++ database/factories/AtletaFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use DateTime;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Atleta>
+ */
+class AtletaFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        $descricao = $this->faker->sentence(10);
+        return [
+            'id' => (string) Str::uuid(),
+            'nome' => $this->faker->name(),
+            'dtNascimento' => $this->dataAtualMenos10Anos(),
+        ];
+    }
+
+    private function dataAtualMenos10Anos() : DateTime
+    {
+        // Cria um objeto DateTime com a data atual
+        $data = new DateTime(today());
+        // Subtrai 100 anos
+        return $data->modify('-10 years');
+    }    
+}
