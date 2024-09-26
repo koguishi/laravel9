4a23814b361c983f5b89de74a6980638956b1dc8
edson koguishi <edsonkoguishi@gmail.com>
2024-09-25 21:36:51 -0300

    test: anotacao para testes dos models

---
+++ tests/Unit/app/Models/CategoriaTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\app\Models;
+
+use PHPUnit\Framework\TestCase;
+
+class CategoriaTest extends TestCase
+{
+    /**
+    * @depends
+    */    
+    public function test_testes_de_models()
+    {
+        dump('warning: ??? implementar testes para o model ? ');
+        $this->assertTrue(true);
+    }
+}
