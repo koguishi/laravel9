848b5244129028268c144a76259ef727f2c68f95
edson koguishi <edsonkoguishi@gmail.com>
2024-11-26 02:07:06 +0000

    migration: indice unico nome da categoria

--- database/migrations/2024_08_22_235332_create_categorias_table.php
+++ database/migrations/2024_08_22_235332_create_categorias_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('categorias', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->string('nome');
+            $table->string('nome')->unique();
             $table->string('descricao')->nullable();
             $table->boolean('ativo')->default(true);
             $table->softDeletes();
